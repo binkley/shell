d1c3f5267ac0dcff6965417c5b4c0cbeac1544c4
Brian Oxley <boxley@thoughtworks.com>
2016-04-07 17:12:37 -0400

    Use EOE for end-of-expectations

--- testing-bash/src/test/resources/t/03-jobs-tests.sh
+++ testing-bash/src/test/resources/t/03-jobs-tests.sh
@@ -4,23 +4,23 @@ scenario "Short option jobs" \
     given_jar a.jar with_jobs 'simple-job' \
         also_jar b.jar with_jobs 'trivial-job' \
     when_run '-j' \
-    then_expect <<'EOJ'
+    then_expect <<'EOE'
 simple-job
 trivial-job
-EOJ
+EOE
 
 scenario "Long option jobs" \
     given_jar a.jar with_jobs 'simple-job' \
         also_jar b.jar with_jobs 'trivial-job' \
     when_run '--jobs' \
-    then_expect <<'EOJ'
+    then_expect <<'EOE'
 simple-job
 trivial-job
-EOJ
+EOE
 
 scenario "Complex jobs help" \
     given_jar a.jar with_jobs 'complex-job -Dfile=$1 $2' \
     when_run '--jobs' \
-    then_expect <<'EOJ'
+    then_expect <<'EOE'
 complex-job -Dfile=$1 $2
-EOJ
+EOE
