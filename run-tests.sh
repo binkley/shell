59e0bf86fcb9f54fbbb82b2afb5c74c98cb065be
Brian Oxley <boxley@thoughtworks.com>
2016-04-19 20:30:58 -0400

    Run tests when possible

--- run-tests.sh
+++ run-tests.sh
@@ -2,26 +2,13 @@
 
 set -e
 
-function run_tests {
-    local d=$1
-    shift
-
-    trap popd RETURN
-    pushd $d
-    if [[ -f Makefile ]]
-    then
-        make
-    elif [[ -f pom.xml ]]
-    then
-        mvn verify
-    else
-        echo "$d: No Makefile or pom.xml"
-        exit 2
-    fi
-}
-
 for d in *
 do
-    [[ -d $d ]] || continue
-    run_tests $d
+    if [[ -f $d/Makefile ]]
+    then
+        (cd $d ; make )
+    elif [[ -f $d/pom.xml ]]
+    then
+        (cd $d ; mvn clean verify)
+    fi
 done
