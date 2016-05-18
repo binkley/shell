f3cb84cd292912d50d124144ce0d400332deef05
Brian Oxley <boxley@thoughtworks.com>
2016-05-18 17:48:19 -0500

    Further caching

--- color.sh
+++ color.sh
@@ -2,6 +2,44 @@
 
 export PS4='+${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): } '
 
+pbold=$(printf '\e[1m')
+plight=$(printf '\e[2m')
+pitalic=$(printf '\e[3m')
+punderscore=$(printf '\e[4m')
+pblink=$(printf '\e[5m')
+preverse=$(printf '\e[7m')
+pinvisible=$(printf '\e[8m')
+pstrikethrough=$(printf '\e[9m')
+
+declare -a effects
+for v in ${!p*}
+do
+    effects+=(${v#p})
+done
+
+preset=$(printf '\e[0m')
+
+pblack=$(printf '\e[30m')
+pred=$(printf '\e[31m')
+pgreen=$(printf '\e[32m')
+pyellow=$(printf '\e[33m')
+pblue=$(printf '\e[34m')
+pmagenta=$(printf '\e[35m')
+pcyan=$(printf '\e[36m')
+pwhite=$(printf '\e[37m')
+printf $preset
+
+for e in ${effects[*]}
+do
+    echo This is $e
+    for c in black red green yellow blue magenta cyan white
+    do
+        eval "printf \" \${p$e}\${p$c}$c\$preset\""
+    done
+    echo
+done
+echo Done
+
 {
     pushd $(dirname $0)
     root_dir=$(pwd -P)
@@ -12,7 +50,7 @@ export PS4='+${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): } '
 [[ -r $X11_RGB_TXT ]] || X11_RGB_TXT=$root_dir/rgb.txt
 
 truecolor=false
-declare -A colors 2>/dev/null && cached_colors=true || cached_colors=false
+declare -A fg_colors 2>/dev/null && cached_colors=true || cached_colors=false
 
 function _truecolor_fg {
     printf '\e[38;2;%d;%d;%dm' $1 $2 $3
@@ -40,10 +78,20 @@ function _256color_bg {
 
 if $cached_colors
 then
+    declare -A bg_colors 2>/dev/null
+    declare -A colors 2>/dev/null
     while read r g b name
     do
         [[ -z "$name" ]] && name="$b" b=0
         colors[$name]="$r $g $b"
+        if $truecolor
+        then
+            fg_colors[$name]=$(_truecolor_fg $r $g $b)
+            bg_colors[$name]=$(_truecolor_bg $r $g $b)
+        else
+            fg_colors[$name]=$(_256color_fg $r $g $b)
+            bg_colors[$name]=$(_256color_bg $r $g $b)
+        fi
     done <$X11_RGB_TXT
     function _to_rgb {
         local -r p="${colors[$1]}"
@@ -85,7 +133,16 @@ function c {
     local r g b
     case $2 in
     [0-9]* ) r=$2 g=$3 b=$3 ;;
-    * ) read r g b < <(_to_rgb $2) ;;
+    * ) if $cached_colors
+        then
+            case $1 in
+            fg ) printf ${fg_colors[$2]} ;;
+            bg ) printf ${bg_colors[$2]} ;;
+            esac
+            return
+        else
+            read r g b < <(_to_rgb $2)
+        fi ;;
     esac
     case $1 in
     fg ) $truecolor && _truecolor_fg $r $g $b || _256color_fg $r $g $b ;;
@@ -95,51 +152,13 @@ function c {
 
 if true
 then
-    for r in $(seq 200 5 255)
+    for r in $(seq 5 10 255)
     do
-        c bg $r 1 1
-        printf '   '
+        c bg $r 0 0
+        printf '  '
     done
     printf '\e[0m\n'
-    echo
 fi
-
-pbold=$(printf '\e[1m')
-plight=$(printf '\e[2m')
-pitalic=$(printf '\e[3m')
-punderscore=$(printf '\e[4m')
-pblink=$(printf '\e[5m')
-preverse=$(printf '\e[7m')
-pinvisible=$(printf '\e[8m')
-pstrikethrough=$(printf '\e[9m')
-preset=$(printf '\e[0m')
-
-declare -a effects
-for v in ${!p*}
-do
-    effects+=(${v#p})
-done
-
-pblack=$(printf '\e[30m')
-pred=$(printf '\e[31m')
-pgreen=$(printf '\e[32m')
-pyellow=$(printf '\e[33m')
-pblue=$(printf '\e[34m')
-pmagenta=$(printf '\e[35m')
-pcyan=$(printf '\e[36m')
-pwhite=$(printf '\e[37m')
-
-
-for e in ${effects[*]}
-do
-    echo This is $e
-    for c in black red green yellow blue magenta cyan white
-    do
-        eval "printf \" \${p$e}\${p$c}$c\$preset\""
-    done
-    echo
-done
-echo Done
 
 for color
 do
