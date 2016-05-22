60ab2fdf886373336ac476c23579a7119e93b4f9
Brian Oxley <boxley@thoughtworks.com>
2016-05-21 21:09:27 -0500

    Separate example from framework

--- plain-bash-testing/f/framework.sh
+++ plain-bash-testing/f/framework.sh
@@ -1,6 +1,10 @@
 # vi: ft=bash
 # Source me
 
+readonly pcheckmark=$(printf "\xE2\x9C\x93")
+readonly pballotx=$(printf "\xE2\x9C\x97")
+readonly pinterrobang=$(printf "\xE2\x80\xBD")
+
 function _print_result {
     local -r exit_code=$1
     $_quiet && return $exit_code
