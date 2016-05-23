f62a4954da46c1ea79c5f72ee89e9e89c0f4532b
Brian Oxley <boxley@thoughtworks.com>
2016-05-23 10:47:47 -0500

    Cleaner

--- plain-bash-testing/simple-example/f/framework.sh
+++ plain-bash-testing/simple-example/f/framework.sh
@@ -2,16 +2,19 @@
 # Source me
 
 readonly pcheckmark=$(printf "\xE2\x9C\x93")
+readonly ppass=$pgreen$pcheckmark$preset
 readonly pballotx=$(printf "\xE2\x9C\x97")
+readonly pfail=$pred$pballotx$preset
 readonly pinterrobang=$(printf "\xE2\x80\xBD")
+readonly perror=$pboldred$pinterrobang$preset
 
 function _print_result {
     local -r exit_code=$1
     $_quiet && return $exit_code
     case $exit_code in
-    0 ) echo -e "$pgreen$pcheckmark$preset $scenario_name" ;;
-    1 ) echo -e "$pred$pballotx$preset $scenario_name - wanted: $expected_color, got $actual_color" ;;
-    * ) echo -e "$pboldred$pinterrobang$preset $scenario_name - exit: $exit_code" ;;
+    0 ) echo -e "$ppass $scenario_name" ;;
+    1 ) echo -e "$pfail $scenario_name - wanted: $expected_color, got $actual_color" ;;
+    * ) echo -e "$perror $scenario_name - exit: $exit_code" ;;
     esac
 }
 
