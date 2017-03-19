243f08d370623ec9907cdc9bb05eee53767864a4
Brian Oxley <boxley@thoughtworks.com>
2017-03-19 13:44:43 -0500

    Test repo is pushable

--- git/f/clauses.sh
+++ git/f/clauses.sh
@@ -12,16 +12,18 @@ function color_of {
 
 # For GIVEN - cannot fail or error, so do not `_register`
 # Global (not local) so visible to later clauses
-trap 'rm -rf $repodir' EXIT  # Must be outside of function
+trap 'rm -rf $upstream $repodir' EXIT  # Must be outside of function
 function a-repo {
     git_tdd=$PWD/git-tdd
+    upstream=$(mktemp -d)
+    git init --bare --quiet $upstream >/dev/null || return $?
     repodir=$(mktemp -d)
-    git init $repodir >/dev/null || return $?
+    git clone --quiet $upstream $repodir >/dev/null 2>&1 || return $?
     (cd $repodir \
         && touch Bob \
         && git add Bob \
-        && git commit -a -m Initial)
-    >/dev/null
+        && git commit --quiet -a -m Initial \
+        && git push --quiet -u origin master) >/dev/null
 }
 _register a-repo
 
