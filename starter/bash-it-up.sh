f8664c82e9fc6cd2ddd460078c6ff1b082d0dd01
Brian Oxley <boxley@thoughtworks.com>
2017-04-18 18:11:08 -0400

    Update starter script
    Color only when requested.

--- starter/bash-it-up.sh
+++ starter/bash-it-up.sh
@@ -1,46 +1,38 @@
 #!/usr/bin/env bash
 
+export PS4='+${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): } '
+
 set -e
 set -u
+set -o pipefail
 
-function setup_colors()
-{
-    # TODO: Should test that 'tput color' > 0?
-    pblack=$(tput setaf 0)
-    pred=$(tput setaf 1)
-    pgreen=$(tput setaf 2)
-    pyellow=$(tput setaf 3)
-    pblue=$(tput setaf 4)
-    pmagenta=$(tput setaf 5)
-    pcyan=$(tput setaf 6)
-    pwhite=$(tput setaf 7)
-    prev=$(tput rev)
-    pbold=$(tput bold)
-    preset=$(tput sgr0)
+function -setup-colors {
+    local -r ncolors=$(tput colors)
+
+    if $color && (( ${ncolors-0} > 7 ))
+    then
+        printf -v pgreen "\e[32m"
+        printf -v preset "\e[0m"
+    else
+        pgreen=''
+        preset=''
+    fi
+    readonly pgreen
+    readonly preset
 }
 
-function enable_debug()
-{
-    export debug=true
-    export PS4='+${BASH_SOURCE}:${LINENO}: ${FUNCNAME[0]:+${FUNCNAME[0]}(): } '
-    set -o pipefail
-    set -o xtrace
+function -enable-debug {
+    set -x
 }
-${debug-false} && enable_debug
 
-# Disable color, etc., if a pipe
-[[ ! -t 1 ]] && export TERM=dumb
-
-function print_usage()
-{
+function -print-usage {
     cat <<EOU
 Usage: $0 [-c|--color][-d|--debug][-h|--help][-n|--dry-run][-v|--verbose]
 EOU
 }
 
-function print_help()
-{
-    print_usage
+function -print-help {
+    -print-usage
     cat <<EOH
 
 Flags:
@@ -52,26 +44,31 @@ Flags:
 EOH
 }
 
+color=false
 debug=false
+print=echo
 pwd=pwd
 verbose=false
 while getopts :-: opt
 do
     [[ - == $opt ]] && opt=${OPTARG%%=*} OPTARG=${OPTARG#*=}
     case $opt in
-    c | color ) setup_colors ;;
-    d | debug ) enable_debug ;;
-    h | help ) print_help ; exit 0 ;;
-    n | dry-run ) pwd="echo $pwd" ;;
+    c | color ) color=true ;;
+    d | debug ) -enable-debug ;;
+    h | help ) -print-help ; exit 0 ;;
+    n | dry-run ) print="echo $print" pwd="echo $pwd" ;;
     v | verbose ) verbose=true ;;
-    * ) print_usage >&2 ; exit 2 ;;
+    * ) -print-usage >&2 ; exit 2 ;;
     esac
 done
 shift $((OPTIND - 1))
 
 case $# in
 0 ) ;;
-* ) print_usage >&2 ; exit 2 ;;
+* ) -print-usage >&2 ; exit 2 ;;
 esac
 
+-setup-colors
+
+$print "${pgreen}I am green.${preset}"
 $pwd
