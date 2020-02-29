b885c5e15de88259f438fbd4340bd2f640baa963
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2020-02-29 09:45:58 -0600

    Show full path to program

--- starter/bash-it-up.sh
+++ starter/bash-it-up.sh
@@ -8,7 +8,6 @@ set -e
 set -u
 set -o pipefail
 
-readonly progname="${0##*/}"
 readonly version=0
 
 : "${LINES:=$(tput lines)}"
@@ -19,56 +18,56 @@ export COLUMNS
 fmt=fmt
 readonly fmt_width=$((COLUMNS - 5))
 function -setup-terminal() {
-  if [[ ! -t 1 ]]; then
-    readonly fmt=cat
-    return 0
-  fi
+    if [[ ! -t 1 ]]; then
+        readonly fmt=cat
+        return 0
+    fi
 
-  if ((fmt_width < 10)); then
-    echo "$progname: Your terminal is too narrow." >&2
-    readonly fmt=cat
-    return 0
-  fi
+    if ((fmt_width < 10)); then
+        echo "$0: Your terminal is too narrow." >&2
+        readonly fmt=cat
+        return 0
+    fi
 
-  fmt="fmt -w $fmt_width"
-  readonly fmt
+    fmt="fmt -w $fmt_width"
+    readonly fmt
 }
 
 function -setup-colors() {
-  local -r ncolors=$(tput colors)
+    local -r ncolors=$(tput colors)
 
-  if $color && ((${ncolors-0} > 7)); then
-    printf -v pgreen "$(tput setaf 2)"
-    printf -v preset "$(tput sgr0)"
-  else
-    pgreen=''
-    preset=''
-  fi
-  readonly pgreen
-  readonly preset
+    if $color && ((${ncolors-0} > 7)); then
+        printf -v pgreen "$(tput setaf 2)"
+        printf -v preset "$(tput sgr0)"
+    else
+        pgreen=''
+        preset=''
+    fi
+    readonly pgreen
+    readonly preset
 }
 
 function -maybe-debug() {
-  case $debug in
-  0) debug=false ;;
-  1) debug=true ;;
-  *)
-    debug=true
-    set -x
-    ;;
-  esac
+    case $debug in
+    0) debug=false ;;
+    1) debug=true ;;
+    *)
+        debug=true
+        set -x
+        ;;
+    esac
 }
 
 function -print-usage() {
-  cat <<EOU | $fmt
-Usage: $progname [OPTION]... [TASK]...
+    cat <<EOU | $fmt
+Usage: $0 [OPTION]... [TASK]...
 EOU
 }
 
 function -print-help() {
-  echo "$progname, version $version"
-  -print-usage
-  cat <<EOH
+    echo "$0, version $version"
+    -print-usage
+    cat <<EOH
 
 Options:
   -c, --color          Print in color
@@ -82,41 +81,41 @@ Options:
 Tasks:
 EOH
 
-  for task in "${tasks[@]}"; do
-    local help_fn="-$task-help"
-    echo "  * $task"
-    if declare -F -- "$help_fn" >/dev/null 2>&1; then
-      $help_fn | -format-help
-    fi
-  done
+    for task in "${tasks[@]}"; do
+        local help_fn="-$task-help"
+        echo "  * $task"
+        if declare -F -- "$help_fn" >/dev/null 2>&1; then
+            $help_fn | -format-help
+        fi
+    done
 }
 
 function -format-help() {
-  $fmt -w $((fmt_width - 8)) | sed 's/^/       /'
+    $fmt -w $((fmt_width - 8)) | sed 's/^/       /'
 }
 
 function -find-in-tasks() {
-  local cmd="$1"
-  for task in "${tasks[@]}"; do
-    [[ "$cmd" == "$task" ]] && return 0
-  done
-  return 1
+    local cmd="$1"
+    for task in "${tasks[@]}"; do
+        [[ "$cmd" == "$task" ]] && return 0
+    done
+    return 1
 }
 
 function -check-cmd() {
-  local cmd="$1"
+    local cmd="$1"
 
-  if ! -find-in-tasks "$cmd"; then
-    echo "$progname: $cmd: Unknown command." >&2
-    echo "Try '$progname --help' for more information." >&2
-    -print-usage >&2
-    exit 2
-  fi
+    if ! -find-in-tasks "$cmd"; then
+        echo "$0: $cmd: Unknown command." >&2
+        echo "Try '$0 --help' for more information." >&2
+        -print-usage >&2
+        exit 2
+    fi
 }
 
 for f in "${0%/*}/functions"/*.sh; do
-  # shellcheck source=functions
-  source "$f"
+    # shellcheck source=functions
+    source "$f"
 done
 
 mapfile -t tasks < <(declare -F | cut -d' ' -f3 | grep -v '^-' | sort)
@@ -129,29 +128,29 @@ readonly tasks
 prefix='> '
 print=echo
 pwd=pwd
-run=  # Nothing, unless dry run
+run= # Nothing, unless dry run
 verbose=false
 while getopts :cdE:hnv-: opt; do
-  [[ $opt == - ]] && opt=${OPTARG%%=*} OPTARG=${OPTARG#*=}
-  case $opt in
-  E | prefix) prefix="$OPTARG" ;;
-  c | color) color=true ;;
-  no-color) color=false ;;
-  d | debug) ((++debug)) ;;
-  h | help)
-    -print-help
-    exit 0
-    ;;
-  n | dry-run)
-    print="echo $prefix$print" pwd="echo $prefix$pwd"
-    run=echo
-    ;;
-  v | verbose) verbose=true ;;
-  *)
-    -print-usage >&2
-    exit 2
-    ;;
-  esac
+    [[ $opt == - ]] && opt=${OPTARG%%=*} OPTARG=${OPTARG#*=}
+    case $opt in
+    E | prefix) prefix="$OPTARG" ;;
+    c | color) color=true ;;
+    no-color) color=false ;;
+    d | debug) ((++debug)) ;;
+    h | help)
+        -print-help
+        exit 0
+        ;;
+    n | dry-run)
+        print="echo $prefix$print" pwd="echo $prefix$pwd"
+        run=echo
+        ;;
+    v | verbose) verbose=true ;;
+    *)
+        -print-usage >&2
+        exit 2
+        ;;
+    esac
 done
 shift $((OPTIND - 1))
 readonly print
@@ -164,11 +163,11 @@ readonly debug
 commands=($(make -f functions/Runfile "$@"))
 
 for cmd in "${commands[@]}"; do
-  if ! -find-in-tasks "$cmd"; then
-    echo "$progname: $cmd: Unknown command." >&2
-    echo "Try '$progname --help' for more information." >&2
-    -print-usage >&2
-    exit 2
-  fi
-  $run "$cmd"
+    if ! -find-in-tasks "$cmd"; then
+        echo "$0: $cmd: Unknown command." >&2
+        echo "Try '$0 --help' for more information." >&2
+        -print-usage >&2
+        exit 2
+    fi
+    $run "$cmd"
 done
